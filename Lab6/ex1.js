ecbb153436949e428b21cfd839a2b2da87c0e2f4
brandonjude <89492957+brandonjude@users.noreply.github.com>
2021-09-21 15:37:35 -1000

    lab 6

---
+++ Lab6/ex1.js
@@ -0,0 +1,13 @@
+var age =  20;
+
+var fav_num = 5;
+
+var day_of_birth = 20;
+
+var month_of_birth = 7;
+
+console.log(age + fav_num / day_of_birth * month_of_birth);
+
+console.log((age + fav_num) / day_of_birth * month_of_birth);
+
+
