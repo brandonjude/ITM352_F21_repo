7846a55369c61273d992024d8e3b63ec5f84e207
brandonjude <89492957+brandonjude@users.noreply.github.com>
2021-10-06 15:28:50 -1000

    lab 8

--- Lab8/ex1.js
+++ Lab8/ex1.js
@@ -1,10 +1,9 @@
 require("./products_data.js");
 
 var num_products = 5;
-
 var count = 1;
-
-while (count <= num_products){
-    console.log(count);
+while (count <= (num_products/2)){
+    console.log(count + ". " + eval("name" + count));
     count++;
 }
+console.log(`That's all we have.`);
