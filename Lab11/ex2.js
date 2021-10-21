aa7f5d171d14f92313200caf486e2ecd2c1c2302
brandonjude <89492957+brandonjude@users.noreply.github.com>
2021-10-20 20:56:04 -1000

    update

---
+++ Lab11/ex2.js
@@ -0,0 +1,12 @@
+var attributes  =  "Brandon;20;20.5;-19.5";
+var parts = attributes.split(";");
+
+
+
+for (part of parts){
+
+    console.log(part, typeof part);
+}
+
+
+console.log(parts.join(","));
