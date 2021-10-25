091e025cc26f2f837d226f841fb7d1e331af650d
brandonjude <89492957+brandonjude@users.noreply.github.com>
2021-10-24 15:42:22 -1000

    update

--- Lab11/ex2.js
+++ Lab11/ex2.js
@@ -1,11 +1,14 @@
 
-
+// create attributes varible containing strings, ints, floats
 var attributes  =  "Brandon;20;20.5;-19.5";
+// split the attributes string by the ; and store them in 'parts'
 var parts = attributes.split(";");
 
+// for each attribute within 'parts'
 for (part of parts){
-
+    //print the type of part
     console.log(part, typeof part);
 }
 
+// concatnetnate parts and seperate by comma
 console.log(parts.join(","));
