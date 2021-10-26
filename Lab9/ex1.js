73de51bf965ddec08bdfafd9bf82ac77d2ada23a
brandonjude <89492957+brandonjude@users.noreply.github.com>
2021-10-26 11:48:45 -1000

    update

---
+++ Lab9/ex1.js
@@ -0,0 +1,25 @@
+var month = 7;
+var day = 20;
+var year = 2001;
+
+step1 = 01;
+
+step2 = parseInt(step1 / 4);
+
+step3 = step2 + step1
+
+step4 = 6;
+
+step6 = step4 + step3;
+
+step7 = day + step6;
+
+step8 = step7;
+
+step9 = step8 -1; // not a leap year
+
+step10 = step9 % 7;
+
+
+
+console.log(step10)
