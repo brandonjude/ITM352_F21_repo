7ac6d61cb634fc357ca392dbad35b2806af60629
brandonjude <89492957+brandonjude@users.noreply.github.com>
2021-11-09 17:21:15 -1000

    update

--- Lab13/info_server_ex2c.js
+++ Lab13/info_server_ex2c.js
@@ -1,13 +1,15 @@
+
 var express = require('express');
 var app = express();
 
 
 
 app.all('*', function (request, response, next) {
-    console.log(request.method + ' to path ' + request.path + " query " + JSON.stringify(request.query));
+    console.log(request.method + ' to path ' + request.path);
     next();
 });
 app.get('/test', function (req, res){
     res.send('Hello my name is Brandon');
 });
+app.use(express.static('./public'));
 app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
