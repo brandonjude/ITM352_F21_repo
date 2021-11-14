fd5e1546dcfa24a558c1b19984ae70bda1ea64b5
brandonjude <89492957+brandonjude@users.noreply.github.com>
2021-11-14 08:08:45 -1000

    update

--- Marcos_BrandonJude_Assignment1/server.js
+++ Marcos_BrandonJude_Assignment1/server.js
@@ -9,9 +9,7 @@ app.all('*', function (request, response, next) {
     console.log(request.method + ' to path ' + request.path + " query " + JSON.stringify(request.body[`quantity_textbox`]));
     next();
 });
-//app.get('/products_display.html', function (req, res){
-    //res.send('Hello my name is Brandon');
-//});
+
 
 app.get("/product_data.js", function (request, response, next) {
     response.type('.js');
@@ -22,56 +20,71 @@ app.get("/product_data.js", function (request, response, next) {
 
 
 
-
 app.post('/invoice', function (req, res, next){
     // receipt = ''; 
     
     quantity_arr = req.body[`quantity_textbox`]; 
+    console.log(quantity_arr);
 
-   /* function isNonNegInt(q) {
+    errors = [];
+    error_product_index = [];
 
-        errors = []; // assume no errors at first
+    function isNonNegInt(q) {
+
         if (q == "") q == 0;
         if (Number(q) != q) {
-            errors.push("Not a number!"); // Check if string is a number value
-            return true;
+            errors.push('not a number!'); // Check if string is a number value
+            error_product_index.push(quantity_arr.indexOf(q));
+            return false;
         }
-        else {
-            if (q < 0) {
-                errors.push("Negative value!"); // Check if it is non-negative
-                return true;
-            }
-            if (parseInt(q) != q) {
-                errors.push("Not an integer!"); // Check that it is an integer
-                return true;
-            }
+        else if (q < 0) {
+            errors.push('a negative value!'); // Check if it is non-negative
+            error_product_index.push(quantity_arr.indexOf(q));
+            return false;
+        }
+        else if (parseInt(q) != q) {
+            errors.push('not an integer!'); // Check that it is an integer
+            error_product_index.push(quantity_arr.indexOf(q));
+            return false;
+        }
 
-        }
+        return true;
     }
 
-    var warning = ``;
+
+   
+
     for (i in quantity_arr) {  
-    
-       if (isNonNegInt(quantity_arr[i]) == true) {
-            console.log(quantity_arr[i], errors);
-            warning += `Your quantity: ${quantity_arr[i]} is ${errors}`;
-       }
-       else {
-        console.log(quantity_arr[i]);
-       }
-    } */
+ 
+       isNonNegInt(quantity_arr[i]);
+       
+    }
+
+    if (errors.length > 0){
+        res.sendFile(__dirname + '/public/qty_error_page.html'); 
+    }
+    else {
+        res.sendFile(__dirname + '/public/invoice.html');
+    }
   
-    res.sendFile(__dirname + '/public/invoice.html');
+    console.log(errors);
+    console.log(error_product_index);
 
 });
 
 app.get("/product_data_quantity.js", function (request, response, next) {
     response.type('.js');
     // quantity_arr = request.body[`quantity_textbox`];
-    console.log(quantity_arr);
     var products_qty_str = `var quantity_arr = [${quantity_arr}];`;
     console.log(products_qty_str);
     response.send(products_qty_str);
+ });
+
+ app.get("/errors_data.js", function (request, response, next) {
+    response.type('.js');
+    var error_str = `var errors = ${JSON.stringify(errors)}; var error_product_index = ${JSON.stringify(error_product_index)};`;
+    console.log(error_str);
+    response.send(error_str);
  });
 
 
